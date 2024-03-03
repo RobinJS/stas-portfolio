bb655aea78478ff9c55fa2e6ead2f52209e1cbb4
RobinJS <peter_velinov@abv.bg>
2024-03-03 19:06:30 +0200

    routes fix

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -4,14 +4,16 @@ import { ArtComponent } from '../art/art.component';
 import { HomeComponent } from '../home/home.component';
 import { AnimationsComponent } from '../animations/animations.component';
 import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'art', component: ArtComponent },
     { path: 'animations', component: AnimationsComponent },
-    { path: 'about', component: AnimationsComponent },
-    { path: 'contact', component: AnimationsComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
     { path: 'not-found', component: PageNotFoundComponent },
     { path: '**', redirectTo: '/not-found' }
 ];
